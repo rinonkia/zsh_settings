47dccbcdf8cc84f0b3c83c4c05a8a062e056ae05
rinonkia <git@gatapon.net>
2021-04-01 02:21:38 +0900

    prompt-customize.shを追加

---
+++ prompt-customize.sh
@@ -0,0 +1,21 @@
+# g-br, g-sh, gh-thisなどをfunctionを定義している
+source ~/.zsh/git-command.sh
+
+# git-promptの読み込み
+source ~/.zsh/git-prompt.sh
+
+# git-completionの読み込み
+fpath=(~/.zsh $fpath)
+zstyle ':completion:*:*:git:*' script ~/.zsh/git-completion.bash
+autoload -Uz compinit && compinit
+
+# プロンプトのオプション表示設定
+GIT_PS1_SHOWDIRTYSTATE=true
+GIT_PS1_SHOWUNTRACKEDFILES=true
+GIT_PS1_SHOWSTASHSTATE=true
+GIT_PS1_SHOWUPSTREAM=auto
+
+# プロンプトの表示設定(好きなようにカスタマイズ可)
+setopt PROMPT_SUBST ; PS1='%F{blue}$(__git_ps1 "(%s)")%f %F{yellow}%~%f
+%F{green}%n@%m%f: \$ '
+
