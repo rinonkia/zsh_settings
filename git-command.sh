59a0890e9422b13716daac83d618bf91635a161e
rinonkia <git@gatapon.net>
2021-04-01 02:00:34 +0900

    git-command.shを追加

---
+++ git-command.sh
@@ -0,0 +1,61 @@
+# git checkout $BRANCH
+# 指定したブランチの削除
+function g-br() {
+    BRANCH=`git branch | peco | tr -d ' '`
+    if [[ -n "$BRANCH" ]]; then
+        git checkout $BRANCH
+    fi
+}
+
+# git branch -D $BRANCH
+# 指定したブランチの削除
+function g-brd() {
+    BRANCH=`git branch | peco | tr -d ' '`
+    if [[ -n "$BRANCH" ]]; then
+        git branch -D $BRANCH
+    fi
+}
+
+function g-cmp() {
+    REPO=`git remote -v | grep fetch | awk -F '[:. ]' '{print $3}'`
+    START=`git log --pretty=oneline | peco | awk '{print $1}'`
+    END=`git log --pretty=oneline | peco | awk '{print $1}'`
+
+    if [[ -z $START || -z $END ]];then
+        echo "canceled comparison."
+        return
+    fi
+
+    # 出力やコピーなど
+    URL="https://github.com/$REPO/compare/$START..$END"
+    echo "[差分]($URL)" | pbcopy
+    open $URL
+}
+
+function g-sh() {
+    HASH=`git log --pretty=oneline | peco | awk '{print $1}'`
+    git show $HASH
+
+    echo "you wanna copy URL(y/N)?: "
+    if read -q; then
+        URL="https://github.com/$REPO/commit/$HASH"
+        echo "[差分]($URL)" | pbcopy
+        echo $URL
+    fi
+}
+
+# github this
+# 現在いるディレクトリがgithubで管理されている場合、
+# そのgithubリポジトリーを開く
+function gh-this() {
+    REPO=`git remote -v | grep fetch | awk -F '[:. ]' '{print $3}'`
+    if [[ -z "$REPO" ]]; then
+        return
+    fi
+
+    URL="https://github.com/$REPO/"
+
+    echo $URL
+    open $URL
+}
+
